604ee5dd3577699b0d8f69fb5aeb4acd041bd67c
0rkunk <154444732+0rkunk@users.noreply.github.com>
2023-12-29 20:41:46 +0300

    Add files via upload

---
+++ Bölenler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumberPrinting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbersDivisibleBy(1, 20, 44);
+            PrintNumbersDivisibleBy(2, 20, 44);
+            PrintNumbersDivisibleBy(3, 20, 44);
+            PrintNumbersDivisibleBy(4, 20, 44);
+            PrintNumbersDivisibleBy(5, 20, 44);
+        }
+
+        static void PrintNumbersDivisibleBy(int divisor, int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (i % divisor == 0)
+                {
+                    Console.Write(i);
+                    if (i + divisor <= end)
+                        Console.Write(" - ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
